b1078a6a52ab797b8cc623951d2f9cd7eb85762d
Darong Mean <darong012@gmail.com>
2019-01-09 14:40:20 +0700

    new module

---
+++ code-interview/src/clj/code_interview/linklists.clj
@@ -0,0 +1,1 @@
+(ns code-interview.linklists)
---
+++ code-interview/test/code_interview/linklists_test.clj
@@ -0,0 +1,7 @@
+(ns code-interview.linklists-test
+  (:require
+    [clojure.test :refer :all]
+    [clojure.test.check.generators :as gen]
+    [com.gfredericks.test.chuck.clojure-test :refer [checking]]
+
+    [code-interview.linklists :as linklists]))
