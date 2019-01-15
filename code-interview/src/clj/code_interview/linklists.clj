f2af8e2d258b8098f1434ebd6f16d7663eac6cea
Darong Mean <darong012@gmail.com>
2019-01-15 21:13:33 +0700

    solution to check if a list is palindrome

--- code-interview/src/clj/code_interview/linklists.clj
+++ code-interview/src/clj/code_interview/linklists.clj
@@ -1,1 +1,43 @@
 (ns code-interview.linklists)
+
+
+(defn slow-fast-pair
+  "The \"runner\" (or second pointer) technique is used in many linked list problems. The runner technique
+  means that you iterate through the linked list with two pointers simultaneously, with one ahead of the other.
+
+  The \"fast\" node might be ahead by a fixed amount, or it might be hopping multiple nodes for each one node
+  that the \"slow\" node iterates through.
+
+  For example, suppose you had a linked list a1 - >a2 - > ••• - >an - >b1 - >b2 - > ••• - >bn and
+  you wanted to rearrange it into a1 - >b1 - >a2 - >b2 - > •.• - >an - >bn•
+  You do not know the length of the linked list (but you do know that the length is an even number).
+  You could have one pointer pl (the fast pointer) move every two elements for everyone move that p2
+  makes. When pl hits the end of the linked list, p2 will be at the midpoint. Then, move pl back to the front
+  and begin \"weaving\" the elements. On each iteration, p2 selects an element and inserts it after pl. "
+  [coll]
+  (let [fast-runner (concat (take-nth 2 (drop 1 (seq coll)))
+                            (repeat nil))
+        slow-runner (seq coll)]
+    (map vector slow-runner fast-runner)))
+
+
+(defn verify-palindrome [{:keys [head tail]}]
+  (every? true? (map = head tail)))
+
+
+(defn collect-head-and-tail
+  "Return a map with the following key:
+  :head a collection of element from the start to the middle(exclude middle if the length is odd)
+  :tail a collection of element from the end to the middle(include middle if the length is odd)"
+  [coll]
+  (let [conj-head-or-tail (fn [acc [x y]] (if (nil? y)
+                                            (update acc :tail conj x)
+                                            (update acc :head conj x)))]
+    (reduce conj-head-or-tail {:head [] :tail (list)} coll)))
+
+
+(defn palindrome? [coll]
+  (-> coll
+      (slow-fast-pair)
+      (collect-head-and-tail)
+      (verify-palindrome)))
--- code-interview/test/code_interview/linklists_test.clj
+++ code-interview/test/code_interview/linklists_test.clj
@@ -1,7 +1,26 @@
 (ns code-interview.linklists-test
   (:require
+    [clojure.spec.alpha :as s]
     [clojure.test :refer :all]
     [clojure.test.check.generators :as gen]
     [com.gfredericks.test.chuck.clojure-test :refer [checking]]
+    [orchestra.spec.test :as st]
 
     [code-interview.linklists :as linklists]))
+
+
+(st/instrument)
+
+
+;;; Implement a function to check if a linked list is a palindrome.
+;;; Assuming the length of the linked list isn't known.
+
+(deftest palindrome?-test
+  (checking "with well known examples" 100
+    [[expected ll] (gen/elements [[false [0 1 2 3]]
+                                  [true [0 1 2 1 0]]
+                                  [true [0 1 1 0]]
+                                  [false [0 1 2 3]]
+                                  [false [0 1 2 3 4]]
+                                  [false [0 1 2 3 4 2 1]]])]
+    (is (= expected (linklists/palindrome? ll)))))
