e432d0f6163b5797400ebb6d1d2457b620ef4d12
Darong Mean <darong012@gmail.com>
2019-01-20 13:03:57 +0700

    implement depth first search algorithm

--- code-interview/src/clj/code_interview/graphs.clj
+++ code-interview/src/clj/code_interview/graphs.clj
@@ -1,2 +1,33 @@
 (ns code-interview.graphs)
 
+
+(defn dfs-itr [{:keys [graph queue] :as m}]
+  (let [[x & xs] queue]
+    (cond
+      (empty? graph) nil
+
+      (empty? queue) nil
+
+      (not (contains? graph x))
+      (-> m
+          (assoc :queue xs))
+
+      (empty? (graph x))
+      (-> m
+          (update :graph dissoc x)
+          (assoc :queue xs)
+          (update :acc conj x))
+
+      :else
+      (-> m
+          (update :graph dissoc x)
+          (assoc :queue (concat (graph x) xs))
+          (update :acc conj x)))))
+
+
+(defn depth-first-search [graph elem]
+  (->> (iterate dfs-itr {:graph graph :queue [elem] :acc []})
+       (take-while (complement nil?))
+       (last)
+       (:acc)))
+
--- code-interview/test/code_interview/graphs_test.clj
+++ code-interview/test/code_interview/graphs_test.clj
@@ -10,3 +10,21 @@
 
 
 (st/instrument)
+
+
+(def directed-graph
+  {0 [1 4 5]
+   1 [3 4]
+   2 [1]
+   3 [2 4]
+   4 []
+   5 []})
+
+
+(deftest depth-first-search-test
+  (is (= [0 1 3 2 4 5] (graphs/depth-first-search directed-graph 0))))
+
+
+;;; Given a directed graph, design an algorithm to find out whether there is
+;;; a route between two nodes.
+
