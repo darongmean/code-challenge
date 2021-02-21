056a12843a73ecda421322bce6bba0d35ea8cb62
Darong Mean <darongmean@users.noreply.github.com>
2021-02-21 14:08:27 +0700

    update deps

--- code-interview/project.clj
+++ code-interview/project.clj
@@ -2,28 +2,22 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url  "https://www.eclipse.org/legal/epl-2.0/"}
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
 
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
   :javac-options ["-target" "11" "-source" "11"]
 
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [expound "0.7.2"]]
+  :dependencies [[org.clojure/clojure "1.10.2"]
+                 [expound "0.8.9"]]
 
   :repl-options {:init-ns code-interview.core}
 
-  :profiles {:dev {:dependencies [[orchestra "2018.12.06-2"]
-                                  [com.gfredericks/test.chuck "0.2.9"]
-                                  [org.clojure/core.rrb-vector "0.0.13"]]
-                   :injections   [(require '[clojure.spec.alpha :as s])
-                                  (require '[expound.alpha :as expound])
-                                  (require '[orchestra.spec.test :as st])
-                                  (set! s/*explain-out* expound/printer)
-                                  (s/check-asserts true)
-                                  (st/instrument)]
-                   :plugins      [[com.jakemccrary/lein-test-refresh "0.23.0"]
-                                  [org.clojure/core.rrb-vector "0.0.13"]
-                                  [venantius/ultra "0.5.2"
-                                   :exclusions [org.clojure/clojure
-                                                org.clojure/core.rrb-vector]]]}})
+  :profiles {:dev {:dependencies [[orchestra "2021.01.01-1"]
+                                  [com.gfredericks/test.chuck "0.2.10"]]
+                   :injections [(require '[clojure.spec.alpha :as s])
+                                (require '[expound.alpha :as expound])
+                                (require '[orchestra.spec.test :as st])
+                                (set! s/*explain-out* expound/printer)
+                                (s/check-asserts true)
+                                (st/instrument)]}})
